c8c5df3b82a62372810ec31cf8aa5cbdc7f6cda5
katy01101011 <katy.91560@gmail.com>
2022-04-20 17:02:30 +0200

    add: "snack2"

--- snack2/js/script.js
+++ snack2/js/script.js
@@ -1,1 +1,18 @@
-alert('ciao')
+// Generatore di "nomi cognomi" casuali: prendendo una lista di nomi e una
+// lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
+const names = ["Harry", "Ermione", "Ron", "Rubeus", "Lucius", "Albus", "Luna", "Drako", "Minerva", "Bellatrix"];
+const surnames = ["Trump", "Obama", "Berlusconi", "Prodi", "Merkel", "Renzi", "Mattarella", "Grillo", "Rydzyk", "Tusk"];
+
+const falseGuest = [];
+
+for (let i = 0; i < 3; i++) {
+    let randomNameIndex = Math.floor(Math.random() * names.length);
+    let randomName = names[randomNameIndex];
+    console.log(randomName);
+    
+    let randomSurnameIndex = Math.floor(Math.random() * surnames.length);
+    let randomSurname = surnames[randomSurnameIndex];
+    console.log(randomSurname);
+
+    console.log(`${randomName} ${randomSurname}`);
+}
