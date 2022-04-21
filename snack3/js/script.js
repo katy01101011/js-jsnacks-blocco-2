4dc95c6e791f667278e3a9946459fe9af7976d8d
katy01101011 <katy.91560@gmail.com>
2022-04-21 09:27:35 +0200

    add: "snack3"

--- snack3/js/script.js
+++ snack3/js/script.js
@@ -1,1 +1,20 @@
-alert("ciao")
+// Stampa il cubo dei primi N numeri,
+// dove N è un numero indicato dall'utente.
+
+const n = parseInt(prompt("Inserisci un numero"));
+const squares = [];
+// let square;
+
+// for (let i = 0; i <= n; i++) {
+//      square = i * i * i;
+//      console.log(square);
+// }
+
+let square;
+let i = 0;
+
+while (i <= n) {
+    square = i * i * i;
+    console.log(square);
+    i++;
+}
